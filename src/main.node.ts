5c5a1ca96060b03e252c17096f4233f445d42503
Jon Harris <harris.jb@gmail.com>
2023-03-23 15:56:05 -0400

    add exports to ts entry files to get declarations...

--- src/main.node.ts
+++ src/main.node.ts
@@ -1,3 +1,13 @@
 import { isEvenFunction, readFileText } from "test-package";
 
 console.log("test-package node isEvenFunction & readFileText: ", isEvenFunction, readFileText);
+
+export { isEvenFunction, readFileText };
+
+export const isNodeEvenFunction = (val: number): boolean => {
+    return isEvenFunction(val);
+};
+
+export const readNodeFileText = (filename: string): string => {
+    return readFileText(filename);
+};
--- src/main.web.ts
+++ src/main.web.ts
@@ -1,3 +1,9 @@
 import { isEvenFunction } from "test-package";
 
 console.log("test-package web isEvenFunction & readFileText: ", isEvenFunction);
+
+export { isEvenFunction };
+
+export const isWebEvenFunction = (val: number): boolean => {
+    return isEvenFunction(val);
+};
